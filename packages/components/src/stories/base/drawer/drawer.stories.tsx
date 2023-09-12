9e1413f2e16521d8d8990875aae7a8a277220566
ali mahmoodi <alimahmudi.alami@gmail.com>
2023-09-12 14:46:56 +0330

    fix undesirable drawer close

--- packages/components/src/stories/base/drawer/drawer.stories.tsx
+++ packages/components/src/stories/base/drawer/drawer.stories.tsx
@@ -1,7 +1,8 @@
 import { Meta, Story } from "@storybook/react/types-6-0";
-import { useState } from "react";
+import { MutableRefObject, useRef, useState } from "react";
 import { Drawer, DrawerProps, Text } from "../../../atoms";
 import { Button } from "../../../molecules";
+import { Select } from "../../../organism";
 import { StoryContainer } from "../../container";
 import React from "react";
 
@@ -10,17 +11,44 @@ export default {
   component: Drawer,
 } as Meta<DrawerProps>;
 
+const fakeData = [...new Array(35)].map((_, index) => {
+  return {
+    value: `TestValue${index}`,
+    label: `Test Label${index}`,
+  };
+});
+
 const Template: Story<DrawerProps> = (args) => {
+  const ref = useRef<HTMLDivElement | null>(null);
   const [isVisible, setVisible] = useState(false);
   const onToggleDrawer = () => {
     setVisible((prev) => !prev);
   };
   return (
     <StoryContainer>
-      <Button onClick={onToggleDrawer}>toggle</Button>
-      <Drawer {...args} isVisible={isVisible} onClose={onToggleDrawer}>
-        <Text>Drawer Content</Text>
-      </Drawer>
+      <div
+        ref={ref}
+        style={{
+          height: 500,
+          border: "1px solid red",
+          overflow: "hidden",
+          position: "relative",
+        }}
+      >
+        <Button onClick={onToggleDrawer}>toggle</Button>
+        <Drawer
+          {...args}
+          isVisible={isVisible}
+          onClose={onToggleDrawer}
+          height={200}
+          getContainer={ref.current}
+        >
+          <div style={{ height: "100%" }}>
+            <Select unit="pixel" data={fakeData} />
+            <Text>Drawer Content</Text>
+          </div>
+        </Drawer>
+      </div>
     </StoryContainer>
   );
 };
